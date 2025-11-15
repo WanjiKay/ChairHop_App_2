03adb6ffe4de497997f539552f518a84e969b5dd
(Sherline Bertrand) <sherlinebertrand@gmail.com>
2025-11-15 15:55:32 -0500

    fixed syntax errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :appointments_as_customer, class_name: "Appointment", foreign_key: customer_id, dependent: :nullify
-  has_many :appointments_as_stylist, class_name: "Appointment", foreign_key: stylist_id, dependent: :nullify
+  has_many :appointments_as_customer, class_name: "Appointment", foreign_key: :customer_id, dependent: :nullify
+  has_many :appointments_as_stylist, class_name: "Appointment", foreign_key: :stylist_id, dependent: :nullify
   has_many :chats
-  has_many :conversations_as_customer, class_name: "Conversation", foreign_key: customer_id, dependent: :nullify
-  has_many :conversations_as_stylist,  class_name: "Conversation", foreign_key: stylist_id, dependent: :nullify
+  has_many :conversations_as_customer, class_name: "Conversation", foreign_key: :customer_id, dependent: :nullify
+  has_many :conversations_as_stylist,  class_name: "Conversation", foreign_key: :stylist_id, dependent: :nullify
 
-  has_many :reviews_for_stylist, class_name: "Review", foreign_key: stylist_id, dependent: :nullify
+  has_many :reviews_for_stylist, class_name: "Review", foreign_key: :stylist_id, dependent: :nullify
 
   has_one_attached :avatar
 end
