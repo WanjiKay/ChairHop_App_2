96ffccfe4e10f140328a64593a4eaf2bc332d84a
Wanji Kay <cirukins@hotmail.com>
2025-11-25 20:06:38 -0500

    chat index issues almost resolved

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,4 +7,22 @@ class Message < ApplicationRecord
   validates :chat, presence: true
   validate :file_size_validation
   validate :user_message_limit, if: -> { role == "user" }
+
+  MAX_FILE_SIZE_MB = 10
+  MAX_USER_MESSAGES = 15
+
+  private
+
+  def file_size_validation
+    if photos.attached? && photos.byte_size > MAX_FILE_SIZE_MB.megabytes
+      errors.add(:file, "size must be less than #{MAX_FILE_SIZE_MB}MB")
+    end
+  end
+
+
+  def user_message_limit
+    if chat.messages.where(role: "user").count >= MAX_USER_MESSAGES
+      errors.add(:base, "You can only send #{MAX_USER_MESSAGES} messages per chat.")
+    end
+  end
 end
