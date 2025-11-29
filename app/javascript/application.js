de112b405dd0ef3945b52e5e0f23e6307d8c3348
Wanji Kay <cirukins@hotmail.com>
2025-11-29 12:32:39 -0500

    updated navbar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,17 @@ import SearchToggleController from "search_toggle_controller"
 
 const application = Application.start()
 application.register("search-toggle", SearchToggleController)
+
+document.addEventListener("DOMContentLoaded", () => {
+  const nav = document.getElementById("mobile-navbar");
+  const heroSection = document.querySelector(".hero-section");
+  const heroHeight = heroSection ? heroSection.offsetHeight : 200;
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > heroHeight) {
+      nav.classList.add("sticky-bottom");
+    } else {
+      nav.classList.remove("sticky-bottom");
+    }
+  });
+});
