68d6eb461e68795baa0bd022be5d96206731e7cb
(Sherline Bertrand) <sherlinebertrand@gmail.com>
2025-11-29 16:05:57 -0500

    added conversation

--- app/models/conversation_message.rb
+++ app/models/conversation_message.rb
@@ -3,4 +3,15 @@ class ConversationMessage < ApplicationRecord
   validates :content, presence: true
   validates :role, presence: true
   validates :conversation, presence: true
+
+  after_create_commit :broadcast_conversation
+
+  private
+
+  def broadcast_conversation
+    broadcast_append_to "conversation_#{conversation.id}_messages",
+                        partial: "conversation_messages/conversation_message",
+                        target: "conversation_messages",
+                        locals: { conversation_message: self }
+  end
 end
