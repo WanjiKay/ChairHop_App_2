3eb25539b7f997bb1b281b9d5f38fb2094e6b6f2
Enrique Ramirez <nrqrmz@gmail.com>
2025-12-12 05:51:45 -0600

    check chat scroll

--- app/javascript/controllers/chat_message_controller.js
+++ app/javascript/controllers/chat_message_controller.js
@@ -4,7 +4,9 @@ export default class extends Controller {
   static targets = ["textarea", "form", "messagesContainer"]
 
   connect() {
-    this.scrollToBottom()
+    requestAnimationFrame(() => {
+      this.scrollToBottom()
+    })
     this.resizeTextarea()
   }
 
