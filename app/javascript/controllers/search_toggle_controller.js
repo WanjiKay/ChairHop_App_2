24f7f9cc86394e3510b6b433a7e4e6ea6cedb921
Enrique Ramirez <nrqrmz@gmail.com>
2025-12-12 03:39:54 -0600

    fix flatpicker calendar rendering styles

--- app/javascript/controllers/search_toggle_controller.js
+++ app/javascript/controllers/search_toggle_controller.js
@@ -12,17 +12,17 @@ export default class extends Controller {
     this.filtersTarget.classList.toggle("d-none");
 
     // Initialize flatpickr only when the form becomes visible
-    if (!this.filtersTarget.classList.contains("d-none")) {
-      this.initFlatpickr();
-    }
+    // if (!this.filtersTarget.classList.contains("d-none")) {
+    //   this.initFlatpickr();
+    // }
   }
 
   initFlatpickr() {
-    if (this.dateTarget && !this.dateTarget._flatpickr) {
-      flatpickr(this.dateTarget, { dateFormat: "Y-m-d" });
-    }
-    if (this.timeTarget && !this.timeTarget._flatpickr) {
-      flatpickr(this.timeTarget, { enableTime: true, noCalendar: true, dateFormat: "H:i" });
-    }
+    // if (this.dateTarget && !this.dateTarget._flatpickr) {
+    //   flatpickr(this.dateTarget, { dateFormat: "Y-m-d" });
+    // }
+    // if (this.timeTarget && !this.timeTarget._flatpickr) {
+    //   flatpickr(this.timeTarget, { enableTime: true, noCalendar: true, dateFormat: "H:i" });
+    // }
   }
 }
