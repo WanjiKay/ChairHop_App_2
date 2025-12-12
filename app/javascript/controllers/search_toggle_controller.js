3405d9c9bf80da987f780f2130fa8def4949ce27
Enrique Ramirez <nrqrmz@gmail.com>
2025-12-12 04:01:27 -0600

    fix issue with flatpickr

--- app/javascript/controllers/search_toggle_controller.js
+++ app/javascript/controllers/search_toggle_controller.js
@@ -5,24 +5,24 @@ export default class extends Controller {
   static targets = ["filters", "date", "time"];
 
   connect() {
-    // Flatpickr se inicializa solo cuando el form se hace visible (en toggle)
+    
   }
 
   toggle() {
     this.filtersTarget.classList.toggle("d-none");
 
     // Initialize flatpickr only when the form becomes visible
-    // if (!this.filtersTarget.classList.contains("d-none")) {
-    //   this.initFlatpickr();
-    // }
+    if (!this.filtersTarget.classList.contains("d-none")) {
+      this.initFlatpickr();
+    }
   }
 
   initFlatpickr() {
-    // if (this.dateTarget && !this.dateTarget._flatpickr) {
-    //   flatpickr(this.dateTarget, { dateFormat: "Y-m-d" });
-    // }
-    // if (this.timeTarget && !this.timeTarget._flatpickr) {
-    //   flatpickr(this.timeTarget, { enableTime: true, noCalendar: true, dateFormat: "H:i" });
-    // }
+    if (this.dateTarget && !this.dateTarget._flatpickr) {
+      flatpickr(this.dateTarget, { dateFormat: "Y-m-d" });
+    }
+    if (this.timeTarget && !this.timeTarget._flatpickr) {
+      flatpickr(this.timeTarget, { enableTime: true, noCalendar: true, dateFormat: "H:i" });
+    }
   }
 }
